9669361908f1f29602fc50a03f66a93230484a3c
maasha96 <54304125+maasha96@users.noreply.github.com>
2019-12-07 15:09:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notekeeperr/NoteListActivity.java
@@ -0,0 +1,55 @@
+package com.example.notekeeperr;
+
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import java.util.List;
+
+public class NoteListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_list2);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+//added code
+        initialiseDisplayContent();
+    }
+
+    private void initialiseDisplayContent() {
+        ListView listNotes = (ListView) findViewById(R.id.note_list);
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        ArrayAdapter<NoteInfo> adapaterNotes = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,notes);
+        listNotes.setAdapter(adapaterNotes);
+
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+    }
+
+}
